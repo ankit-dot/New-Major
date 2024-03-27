b982e26c85ece9b0a39997e7642415dcafa4da2a
Ankit Solanki <ankitsolanki61375@gmail.com>
2024-03-27 16:41:12 +0530

    improvement

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,23 +1,38 @@
-//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
-
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import express from "express"
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const router = express.Router();
-const genAI = new GoogleGenerativeAI('AIzaSyB_sq06j-lTVXBZ-oElRomKqUGT3rlRwUc');
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY);
 
-router.get('/ask-question', async (req, res) => {
+router.post('/ask-question', async (req, res) => {
   const model = genAI.getGenerativeModel({model: "gemini-pro"});
 
-  const prompt = "write a essay on diwali";
+  if (typeof req.body.question !== 'string') {
+    res.status(400).json({ error: 'Question must be a string' });
+    return;
+  }
+
+  const prompt = req.body.question;
   
-  const result = await model.generateContent(prompt);
-
-  const response = result.response;
-  const text = response ? response.text() : null;
-  res.json(text);
-  console.log(text);
+  try {
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response ? response.text() : null;
+    if (response) {
+      res.json(text);
+      console.log(text);
+    } else {
+      res.json(null);
+      console.log(null);
+    }
+  } catch (error) {
+    // Handle the error here
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
-
 
 export default router;
